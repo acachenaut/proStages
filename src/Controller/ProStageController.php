622c2cbf36be2279c674134a35815f1dce651233
Aitor Cachenaut <acachenautiutbayonne.univ-pau.fr>
2019-11-12 17:02:06 +0100

    Codage des messages de chaque page

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -16,4 +16,37 @@ class ProStageController extends AbstractController
             'controller_name' => 'ProStageController',
         ]);
     }
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function afficherMessageAccueil()
+    {
+        return new Reponse('<html><body><h1>Bienvenu sur la page d\'accueil de Prostages</h1></body></html>');
+    }
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+
+    public function afficherMessageEntreprises()
+    {
+        return new Reponse('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
+    }
+    /**
+     * @Route("/formations", name="formations")
+     */
+
+    public function afficherMessageFormations()
+    {
+        return new Reponse('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
+    }
+    /**
+     * @Route("/stages{id}", name="descriptifStage")
+     */
+     
+    public function afficherDescriptifStage()
+    {
+        return new Reponse('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant id</h1></body></html>');
+    }
+
+
 }
