22d2926e50f2764e3b125e9fdbac952e02f74d16
Aitor Cachenaut <acachenautiutbayonne.univ-pau.fr>
2019-11-19 16:15:41 +0100

    Fin du TP1

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
@@ -21,7 +22,7 @@ class ProStageController extends AbstractController
      */
     public function afficherMessageAccueil()
     {
-        return new Reponse('<html><body><h1>Bienvenu sur la page d\'accueil de Prostages</h1></body></html>');
+        return new Response('<html><body><h1>Bienvenu sur la page d\'accueil de Prostages</h1></body></html>');
     }
     /**
      * @Route("/entreprises", name="entreprises")
@@ -29,7 +30,7 @@ class ProStageController extends AbstractController
 
     public function afficherMessageEntreprises()
     {
-        return new Reponse('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
+        return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
     }
     /**
      * @Route("/formations", name="formations")
@@ -37,15 +38,15 @@ class ProStageController extends AbstractController
 
     public function afficherMessageFormations()
     {
-        return new Reponse('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
+        return new Response('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
     }
     /**
-     * @Route("/stages{id}", name="descriptifStage")
+     * @Route("/stages/{id}", name="descriptifStage")
      */
-     
-    public function afficherDescriptifStage()
+
+    public function afficherDescriptifStage($id)
     {
-        return new Reponse('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant id</h1></body></html>');
+        return new Response('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant '.$id.'</h1></body></html>');
     }
 
 
