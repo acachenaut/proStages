818f2f09a2277bc7366f5626e7eb55eafd790db4
Aitor Cachenaut <acachenautiutbayonne.univ-pau.fr>
2019-11-22 15:01:27 +0100

    Fin du TP1 avec la mise en place des routes externalisées et des vues

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,45 +8,38 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProStageController extends AbstractController
 {
-    /**
-     * @Route("/pro/stage", name="pro_stage")
-     */
+
     public function index()
     {
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
         ]);
     }
-    /**
-     * @Route("/", name="accueil")
-     */
-    public function afficherMessageAccueil()
-    {
-        return new Response('<html><body><h1>Bienvenu sur la page d\'accueil de Prostages</h1></body></html>');
-    }
-    /**
-     * @Route("/entreprises", name="entreprises")
-     */
 
-    public function afficherMessageEntreprises()
+    public function afficherAccueil()
     {
-        return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
+      return $this->render('pro_stage/accueil.html.twig');
     }
-    /**
-     * @Route("/formations", name="formations")
-     */
 
-    public function afficherMessageFormations()
+
+    public function afficherEntreprises()
     {
-        return new Response('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
+        return $this->render('pro_stage/entreprises.html.twig');
+
     }
-    /**
-     * @Route("/stages/{id}", name="descriptifStage")
-     */
+
+
+    public function afficherFormations()
+    {
+        return $this->render('pro_stage/formations.html.twig');
+    }
+
 
     public function afficherDescriptifStage($id)
     {
-        return new Response('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant '.$id.'</h1></body></html>');
+      return $this->render('pro_stage/stages.html.twig', [
+          'idRessources' => $id
+      ]);
     }
 
 
